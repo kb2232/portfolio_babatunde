4ee0ba1d079bbfd4816b3a4e0a015856835a507c
kunle babatunde <kb2232@nyu.edu>
2019-02-24 21:19:02 -0500

    navbar icon

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
     $(this).css('background-color','#ff0000')
   })
 
+  $('.js-nav-icon').click(function(){
+    let nav = $('.main_nav');
+    nav.slideToggle(200)
+  })
+
 });
