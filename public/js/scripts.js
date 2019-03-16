0d5079d2ee2f7077a5f344ffaf66b9a3f3495dd8
kunle babatunde <kb2232@nyu.edu>
2019-03-16 12:15:37 -0400

    more canvas

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -15,14 +15,17 @@ function init()
 {
   let stage = new createjs.Stage("stage-canvas")
   let circle = new createjs.Shape();
-  circle.graphics.beginFill("red");
-  circle.graphics.drawCircle(0,0,25);
+  circle.graphics.beginFill("#f1c40f");
+  circle.graphics.drawCircle(0,0,15);
   circle.graphics.endFill();
   circle.x = 30;
-  circle.y = 70;
+  circle.y = 100;
   stage.addChild(circle);
   createjs.Tween.get(circle, { loop: true })
-    .to({ x: 300 }, 2000, createjs.Ease.getPowInOut(4));
+    .to( { x: 250 }, 2000, createjs.Ease.getPowInOut(4))
+    .to( { y: 50 }, 1000, createjs.Ease.getPowInOut(2))
+    .to( { x: -5 }, 2000, createjs.Ease.getPowInOut(4))
+    .to( { y: 150 }, 1000, createjs.Ease.getPowInOut(2))
   createjs.Ticker.setFPS(60);
   createjs.Ticker.addEventListener("tick", stage);
 }
